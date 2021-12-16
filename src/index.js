d993c206c186fa7074dc318e6cd172f27f4c39ef
AndreaAguado <andrea95am@gmail.com>
2021-12-16 11:40:55 +0100

    add endpoint to get all books from db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const DataBase = require('better-sqlite3');
+const req = require('express/lib/request');
 
 // create server
 const server = express();
@@ -17,6 +18,15 @@ const serverPort = 3000;
 server.listen(serverPort, () => {
     console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// endpoints
+
+// get all books 
+server.get('/books', (req, res) => {
+    const query = db.prepare('SELECT * from books');
+    const books = query.all();
+    res.json(books);
+})
 
 // STATIC SERVER: listen files in public folder
 const staticServerPath = './public'; // relative to the root of the project
