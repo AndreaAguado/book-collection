15e3c5ab461a76d19c42e1c2319ce3c9f848c507
AndreaAguado <andrea95am@gmail.com>
2021-12-16 22:30:53 +0100

    add styles to form

--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,13 +1,15 @@
+import '../styles/layout/Form.scss';
+
 const Form = () => {
     return (
-        <form action="POST">
-            <label htmlFor="title">Title *</label>
+        <form className="form" method="POST">
+            <label htmlFor="title">Title <span className="form_asterisk">*</span></label>
             <input type="text" placeholder="Romeo and Juliet" name="title" id="title" required />
-            <label htmlFor="first_name">First name *</label>
+            <label htmlFor="first_name">First name <span className="form_asterisk">*</span></label>
             <input type="text" placeholder="William" name="first_name" id="first_name" required />
-            <label htmlFor="last_name">Last name *</label>
+            <label htmlFor="last_name">Last name <span className="form_asterisk"><span className="form_asterisk">*</span></span></label>
             <input type="text" placeholder="Shakespeare" name="last_name" id="last_name" required />
-            <label htmlFor="isbn">ISBN *</label>
+            <label htmlFor="isbn">ISBN <span className="form_asterisk">*</span></label>
             <input type="text" placeholder="978-1451552058" name="isbn" id="isbn" required />
             <input type="submit" />
         </form>
