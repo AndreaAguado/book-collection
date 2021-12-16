c77d588aca8337d4b3cc659bbcc51f553d8d79b4
AndreaAguado <andrea95am@gmail.com>
2021-12-16 18:23:08 +0100

    add footer html structure

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,6 +1,24 @@
 import '../styles/App.scss';
 const App = () => {
-  return <div className="page">Hola mundo</div>;
+  return (
+    <div className="page">
+      <header className="header">
+
+      </header>
+      <main className="main">
+        <h1>Book Collection</h1>
+      </main>
+      <footer className="footer">
+        <nav className="footer_nav">
+          <ul className="footer_links_list">
+            <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
+            <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-moleon/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className="fab fa-github"></i></a></li>
+          </ul>
+        </nav>
+        <small className="footer_small">Hecho con <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
+      </footer>
+    </div>);
 };
 
 export default App;
