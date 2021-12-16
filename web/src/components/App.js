6faabce5c6ef5eaef7a1209131a502eb62d018f7
AndreaAguado <andrea95am@gmail.com>
2021-12-16 18:29:00 +0100

    add Footer component and import styles

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,4 +1,5 @@
 import '../styles/App.scss';
+import Footer from './Footer';
 const App = () => {
   return (
     <div className="page">
@@ -8,16 +9,7 @@ const App = () => {
       <main className="main">
         <h1>Book Collection</h1>
       </main>
-      <footer className="footer">
-        <nav className="footer_nav">
-          <ul className="footer_links_list">
-            <li><a className="footer_link" href="https://twitter.com/andrea114am" target="_blank" rel="noreferrer" title="twitter.com/andrea114am"><i className="fab fa-twitter"></i></a></li>
-            <li><a className="footer_link" href="https://www.linkedin.com/in/andrea-aguado-moleon/" target="_blank" rel="noreferrer" title="linkedin.com/in/andrea-aguado-moleón/"><i className="fab fa-linkedin-in"></i></a></li>
-            <li><a className="footer_link" href="https://github.com/AndreaAguado" target="_blank" rel="noreferrer" title="github.com/AndreaAguado"><i className="fab fa-github"></i></a></li>
-          </ul>
-        </nav>
-        <small className="footer_small">Hecho con <span role="img" aria-label="emoji corazón" title="emoji corazón">&#10084;&#65039;</span> &copy; Andrea Aguado 2021</small>
-      </footer>
+      <Footer></Footer>
     </div>);
 };
 
