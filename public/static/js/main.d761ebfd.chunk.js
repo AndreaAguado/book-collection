e64dc8ee03302cb0759e40650a7a5cea605a5390
AndreaAguado <andrea95am@gmail.com>
2021-12-17 17:58:05 +0100

    update production vers

--- public/static/js/main.d761ebfd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-starter-kit"]=this["webpackJsonpreact-starter-kit"]||[]).push([[0],{22:function(t,e,r){"use strict";r.r(e);r(0);var n=r(8),c=r.n(n),s=r(11),a=r(3),i=function(){return Object(a.jsx)("div",{children:"Hola mundo"})};c.a.render(Object(a.jsx)(s.a,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))}},[[22,1,2]]]);
-//# sourceMappingURL=main.d761ebfd.chunk.js.map
