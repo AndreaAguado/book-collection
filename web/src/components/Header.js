5963925d4fd5071cf592bf8e7186365973a9deff
AndreaAguado <andrea95am@gmail.com>
2021-12-18 20:44:08 +0100

    change header logo to local image

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,8 +1,9 @@
 import '../styles/layout/Header.scss';
+import headerLogo from '../images/header_logo.png';
 const Header = () => {
     return (
         <header className="header">
-            <img className="header_logo" src="https://i2.wp.com/lblegalservices.com.mx/wp-content/uploads/2015/06/libros-de-derecho-3.png?ssl=1" alt="header logo" />
+            <img className="header_logo" src={headerLogo} alt="header logo" />
             <h1 className="header_title">Book Collection</h1>
         </header>
     )
