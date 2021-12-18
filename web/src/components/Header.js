342cb7a33fd292f2d1ef60cd9b63affb81a5ee38
AndreaAguado <andrea95am@gmail.com>
2021-12-18 21:17:51 +0100

    add link to homepage in header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,9 +1,12 @@
 import '../styles/layout/Header.scss';
 import headerLogo from '../images/header_logo.png';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="header">
-            <img className="header_logo" src={headerLogo} alt="header logo" />
+            <Link to="/" className="header_link" title="Go to homepage">
+                <img className="header_logo" src={headerLogo} alt="header logo" />
+            </Link>
             <h1 className="header_title">Book Collection</h1>
         </header>
     )
