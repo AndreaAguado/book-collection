e15f78425bb5551250c16a2c8eea55ca436c4a98
AndreaAguado <andrea95am@gmail.com>
2021-12-18 22:46:09 +0100

    change positions in book details

--- web/src/components/BookDetail.js
+++ web/src/components/BookDetail.js
@@ -7,24 +7,25 @@ const BookDetail = (props) => {
     return (
         <>
 
-            <section className="book_detail">
-                <div>
-                    <SectionWrapper classname="book_detail_info" section_name={`Title: ${props.bookToRender.title}`}>
-                        <h3>Author: {props.bookToRender.author}</h3>
-                        <p>ISBN: {props.bookToRender.isbn}</p>
+            <main>
+                <section className="book_detail">
+                    <div className="book_detail_container">
+                        <SectionWrapper classname="book_detail_info" section_name={`Title: ${props.bookToRender.title}`}>
+                            <h3>Author: {props.bookToRender.author}</h3>
+                            <p>ISBN: {props.bookToRender.isbn}</p>
+                        </SectionWrapper>
+                        <nav className="return_link_container">
+                            <Link to="/" className="return_link" title="Go to homepage">
+                                <i class="return_link_icon fas fa-chevron-circle-left"></i>
+                                <p className="return_link_title">Return</p>
+                            </Link>
+                        </nav>
+                    </div>
+                    <SectionWrapper classname="update_book" section_name="Update book info">
+                        <Form handleInput={props.handleInput}></Form>
                     </SectionWrapper>
-                    <nav className="return_link_container">
-                        <Link to="/" className="return_link" title="Go to homepage">
-                            <i class="return_link_icon fas fa-chevron-circle-left"></i>
-                            <p className="return_link_title">Return</p>
-                        </Link>
-                    </nav>
-                </div>
-
-                <SectionWrapper classname="update_book" section_name="Update book info">
-                    <Form handleInput={props.handleInput}></Form>
-                </SectionWrapper>
-            </section>
+                </section>
+            </main>
         </>
 
     )
