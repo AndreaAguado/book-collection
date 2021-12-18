da88b941edb9716ddddb10c8177ae8238945bb97
AndreaAguado <andrea95am@gmail.com>
2021-12-18 22:28:10 +0100

    adjust bg image in book detail

--- web/src/components/BookDetail.js
+++ web/src/components/BookDetail.js
@@ -1,15 +1,26 @@
 import '../styles/layout/BookDetail.scss';
 import SectionWrapper from './SectionWrapper';
 import Form from './Form';
+import { Link } from 'react-router-dom';
 
 const BookDetail = (props) => {
     return (
         <>
+
             <section className="book_detail">
-                <SectionWrapper classname="book_detail_info" section_name={`Title: ${props.bookToRender.title}`}>
-                    <h3>Author: {props.bookToRender.author}</h3>
-                    <p>ISBN: {props.bookToRender.isbn}</p>
-                </SectionWrapper>
+                <div>
+                    <SectionWrapper classname="book_detail_info" section_name={`Title: ${props.bookToRender.title}`}>
+                        <h3>Author: {props.bookToRender.author}</h3>
+                        <p>ISBN: {props.bookToRender.isbn}</p>
+                    </SectionWrapper>
+                    <nav className="return_link_container">
+                        <Link to="/" className="return_link" title="Go to homepage">
+                            <i class="return_link_icon fas fa-chevron-circle-left"></i>
+                            <p className="return_link_title">Return</p>
+                        </Link>
+                    </nav>
+                </div>
+
                 <SectionWrapper classname="update_book" section_name="Update book info">
                     <Form handleInput={props.handleInput}></Form>
                 </SectionWrapper>
