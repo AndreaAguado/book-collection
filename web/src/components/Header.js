2db913b28c53d4abf64398a3be5f7e37838b04fc
AndreaAguado <andrea95am@gmail.com>
2021-12-18 20:39:47 +0100

    change header structure and add styles

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -2,7 +2,8 @@ import '../styles/layout/Header.scss';
 const Header = () => {
     return (
         <header className="header">
-            <h1>Book Collection</h1>
+            <img className="header_logo" src="https://i2.wp.com/lblegalservices.com.mx/wp-content/uploads/2015/06/libros-de-derecho-3.png?ssl=1" alt="header logo" />
+            <h1 className="header_title">Book Collection</h1>
         </header>
     )
 }
