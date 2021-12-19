08b0ccaff37dd0bdbf77a89538bab3a95a89bdc3
AndreaAguado <andrea95am@gmail.com>
2021-12-19 19:58:13 +0100

    change form labels

--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -22,9 +22,9 @@ const Form = (props) => {
         <form className="form" method="POST" onSubmit={ev => ev.preventDefault()}>
             <label htmlFor="name">Title <span className="form_asterisk">*</span></label>
             <input onChange={handleChange} type="text" placeholder="Romeo and Juliet" name="name" id="name" required />
-            <label htmlFor="first_name">First name <span className="form_asterisk">*</span></label>
+            <label htmlFor="first_name">Author's first name <span className="form_asterisk">*</span></label>
             <input onChange={handleChange} type="text" placeholder="William" name="first_name" id="first_name" required />
-            <label htmlFor="last_name">Last name <span className="form_asterisk"><span className="form_asterisk">*</span></span></label>
+            <label htmlFor="last_name">Author's last name <span className="form_asterisk"><span className="form_asterisk">*</span></span></label>
             <input onChange={handleChange} type="text" placeholder="Shakespeare" name="last_name" id="last_name" required />
             <label htmlFor="isbn">ISBN <span className="form_asterisk">*</span></label>
             <input onChange={handleChange} type="text" placeholder="978-1451552058" name="isbn" id="isbn" required />
